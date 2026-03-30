543a8bf17fad4409a9944ef6324ba3348d3ba2c1
Giantvince1 <giantvince1@protonmail.com>
2026-03-30 03:38:02 -0500

    Add support for 5 mods: Farmer's Delight, More Delight, Illager Invasion, Late Game Plus, Tough As Nails
    Signed-off-by: Giantvince1 <giantvince1@protonmail.com>

---
+++ src/main/java/net/fneifnox/customdurability/compat/FarmersDelightCompat.java
@@ -0,0 +1,61 @@
+package net.fneifnox.customdurability.compat;
+
+import net.fabricmc.fabric.api.item.v1.DefaultItemComponentEvents;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.util.Unit;
+import net.minecraft.world.item.Item;
+
+import static net.fneifnox.customdurability.CustomDurability.CONFIG;
+import static net.minecraft.core.component.DataComponents.MAX_DAMAGE;
+import static net.minecraft.core.component.DataComponents.UNBREAKABLE;
+
+public class FarmersDelightCompat {
+
+    public static void setDurabilityAndUnbreakableKnives() {
+        DefaultItemComponentEvents.MODIFY.register(context -> {
+            Item flintKnife     = BuiltInRegistries.ITEM.getValue(Identifier.fromNamespaceAndPath("farmersdelight", "flint_knife"));
+            Item copperKnife    = BuiltInRegistries.ITEM.getValue(Identifier.fromNamespaceAndPath("farmersdelight", "copper_knife"));
+            Item ironKnife      = BuiltInRegistries.ITEM.getValue(Identifier.fromNamespaceAndPath("farmersdelight", "iron_knife"));
+            Item goldenKnife    = BuiltInRegistries.ITEM.getValue(Identifier.fromNamespaceAndPath("farmersdelight", "golden_knife"));
+            Item diamondKnife   = BuiltInRegistries.ITEM.getValue(Identifier.fromNamespaceAndPath("farmersdelight", "diamond_knife"));
+            Item netheriteKnife = BuiltInRegistries.ITEM.getValue(Identifier.fromNamespaceAndPath("farmersdelight", "netherite_knife"));
+
+            if (!CONFIG.farmersDelight.unbreakableFlintKnife()) {
+                context.modify(flintKnife, components -> components.set(MAX_DAMAGE, CONFIG.farmersDelight.durabilityForFlintKnife()));
+            } else {
+                context.modify(flintKnife, components -> components.set(UNBREAKABLE, Unit.INSTANCE));
+            }
+
+            if (!CONFIG.farmersDelight.unbreakableCopperKnife()) {
+                context.modify(copperKnife, components -> components.set(MAX_DAMAGE, CONFIG.farmersDelight.durabilityForCopperKnife()));
+            } else {
+                context.modify(copperKnife, components -> components.set(UNBREAKABLE, Unit.INSTANCE));
+            }
+
+            if (!CONFIG.farmersDelight.unbreakableIronKnife()) {
+                context.modify(ironKnife, components -> components.set(MAX_DAMAGE, CONFIG.farmersDelight.durabilityForIronKnife()));
+            } else {
+                context.modify(ironKnife, components -> components.set(UNBREAKABLE, Unit.INSTANCE));
+            }
+
+            if (!CONFIG.farmersDelight.unbreakableGoldenKnife()) {
+                context.modify(goldenKnife, components -> components.set(MAX_DAMAGE, CONFIG.farmersDelight.durabilityForGoldenKnife()));
+            } else {
+                context.modify(goldenKnife, components -> components.set(UNBREAKABLE, Unit.INSTANCE));
+            }
+
+            if (!CONFIG.farmersDelight.unbreakableDiamondKnife()) {
+                context.modify(diamondKnife, components -> components.set(MAX_DAMAGE, CONFIG.farmersDelight.durabilityForDiamondKnife()));
+            } else {
+                context.modify(diamondKnife, components -> components.set(UNBREAKABLE, Unit.INSTANCE));
+            }
+
+            if (!CONFIG.farmersDelight.unbreakableNetheriteKnife()) {
+                context.modify(netheriteKnife, components -> components.set(MAX_DAMAGE, CONFIG.farmersDelight.durabilityForNetheriteKnife()));
+            } else {
+                context.modify(netheriteKnife, components -> components.set(UNBREAKABLE, Unit.INSTANCE));
+            }
+        });
+    }
+}
