1e77e6f208229b56ee3a02d973c4f367e0d70857
Fneifnox <tim.stier9@gmail.com>
2026-04-01 11:14:49 +0200

    3.3.0 Update

--- src/main/java/net/fneifnox/customdurability/compat/IllagerInvasionCompat.java
+++ src/main/java/net/fneifnox/customdurability/compat/IllagerInvasionCompat.java
@@ -1,10 +1,10 @@
 package net.fneifnox.customdurability.compat;
 
 import net.fabricmc.fabric.api.item.v1.DefaultItemComponentEvents;
+import net.minecraft.component.type.UnbreakableComponent;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Unit;
 
 import static net.fneifnox.customdurability.CustomDurability.CONFIG;
 import static net.minecraft.component.DataComponentTypes.MAX_DAMAGE;
@@ -19,7 +19,7 @@ public class IllagerInvasionCompat {
             if (!CONFIG.illagerInvasion.unbreakablePlatinumInfusedHatchet()) {
                 context.modify(platinumInfusedHatchet, components -> components.add(MAX_DAMAGE, CONFIG.illagerInvasion.durabilityForPlatinumInfusedHatchet()));
             } else {
-                context.modify(platinumInfusedHatchet, components -> components.add(UNBREAKABLE, Unit.INSTANCE));
+                context.modify(platinumInfusedHatchet, components -> components.add(UNBREAKABLE, new UnbreakableComponent(true)));
             }
         });
     }
